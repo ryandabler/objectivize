63a11da2326a041bf4330419c3331c3b361ac0e1
Ryan Dabler <rpdabler@gmail.com>
2018-06-11 20:31:34 -0600

    implement duplicateObject()

--- src/duplicate.js
+++ src/duplicate.js
@@ -6,8 +6,19 @@ const duplicateNumber = num => num
 
 const duplicateDate = dt => new Date(dt)
 
+const duplicateObject = obj => {
+    const retObj = {};
+
+    for (const key in obj) {
+        retObj[key] = duplicate[typeOf(obj[key])](obj[key]);
+    }
+
+    return retObj;
+}
+
 const duplicate = {
     [type.String]: duplicateString,
     [type.Number]: duplicateNumber,
     [type.Date]: duplicateDate,
+    [type.Object]: duplicateObject
 }
