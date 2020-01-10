4ccbfd5aaad6535012f93206f8c3d9f07b5361f3
Ryan Dabler <rpdabler@gmail.com>
2020-01-09 20:24:35 -0700

    add a lot of documentation

--- src/decorators.js
+++ src/decorators.js
@@ -1,3 +1,13 @@
+/**
+ * Accepts an arbitrary list of mapping functions and returns a decorator function.
+ * 
+ * The decorator will intercept the parameters and pass them into their respective
+ * mapping function (based on index). If more parameters are supplied to the
+ * decoratee, the mapping functions will fall back to the identity function.
+ * 
+ * @param  {...Function} normalizerFns 
+ * @returns {Function}
+ */
 const normalizeParams = (...normalizerFns) => fn =>
   function withNormalizedParams(..._params) {
     const params = _params.map((param, idx) => {
@@ -8,6 +18,19 @@ const normalizeParams = (...normalizerFns) => fn =>
     return fn.call(this, ...params);
   };
 
+/**
+ * Accepts an arbitrary list of validating functions and a function to run if
+ * any parameter is invalid.
+ * 
+ * The decorator will intercept the parameters and ensure that each parameter
+ * passes its respective validating function (based on index). If any paramter
+ * fails, an `onInvalid` function is run with the given parameters passed in.
+ * This function requires all parameters to be validated.
+ * 
+ * @param {Function} onInvalid 
+ * @param  {...Function} ensurerFns 
+ * @returns {Function}
+ */
 const ensureParams = (onInvalid, ...ensurerFns) => fn =>
     function withEnsuredParams(...params) {
         const isValid = ensurerFns.length === params.length
--- src/utilities.js
+++ src/utilities.js
@@ -37,10 +37,35 @@ const isKeyed = isOneOf(
     $FLOAT64ARRAY
 );
 
+/**
+ * Checks of all arguments are objects.
+ * 
+ * @param  {...any} values 
+ * @returns {boolean}
+ */
 const areObjects = (...values) => values.every($OBJECT);
 
+/**
+ * Checks if an argument represents a valid object path.
+ * 
+ * Possible paths are either arrays of string/number/symbol values
+ * or individual strings, numbers, or symbols.
+ * 
+ * @param {*}
+ * @returns {boolean}
+ */
 const isValidPath = isOneOf(isArrayOf($STRING, $NUMBER, $SYMBOL), $STRING, $NUMBER, $SYMBOL);
 
+/**
+ * Recursively traverses an object and returns the value found at the end of a path.
+ * 
+ * This function assumes that `path` is normalized as an array of numbers, strings, and
+ * symbols.
+ * 
+ * @param {Object} obj 
+ * @param {Array<string | number | symbol>} path 
+ * @returns {any}
+ */
 const _get = (obj, path) => {
     if (path.length === 0) return obj;
     if (!isKeyed(obj)) return undefined;
@@ -48,6 +73,17 @@ const _get = (obj, path) => {
     return _get(obj[path[0]], path.slice(1));
 };
 
+/**
+ * Recursively traverses an object and sets `val` at the end of the specified path.
+ * 
+ * This function assumes that `path` has been normalized to be an array of strings,
+ * numbers, and symbols. Returns `true` if set has successful and `false` otherwise.
+ * 
+ * @param {Object} obj 
+ * @param {Array<string | number | symbol>} path 
+ * @param {*} val 
+ * @returns {boolean}
+ */
 const _set = (obj, path, val) => { 
     if (!isKeyed(obj)) return false;
 
@@ -61,6 +97,15 @@ const _set = (obj, path, val) => {
     return _set(obj[path[0]], path.slice(1), val);
 };
 
+/**
+ * Recursively traverses an object and deletes a key at the specified path.
+ * 
+ * Assumes that `path` has been normalized to be an array of strings, numbers,
+ * and symbols. Returns `true` if deletion was successful and `false` otherwise.
+ * @param {Object} obj 
+ * @param {Array<string | number | symbol>} path 
+ * @returns {boolean}
+ */
 const _delete = (obj, path) => { 
     if (!isKeyed(obj)) return false;
 
@@ -72,6 +117,17 @@ const _delete = (obj, path) => {
     return _delete(obj[path[0]], path.slice(1));
 };
 
+/**
+ * Recursively traverses an object and checks for the existence of a key at a
+ * specified path.
+ * 
+ * Assumes `path` has been normalized to be an array of strings, numbers, and
+ * symbols. Returns `true` if something exists, and `false` otherwise.
+ * 
+ * @param {Object} obj 
+ * @param {Array<string | number | path>} path 
+ * @returns {boolean}
+ */
 const _has = (obj, path) => {
     if (!isKeyed(obj)) return false;
     
@@ -82,11 +138,45 @@ const _has = (obj, path) => {
     return _has(obj[path[0]], path.slice(1));
 };
 
+/**
+ * Simulates `Object.assign` for merging the second object's value onto the
+ * primary objects value. Used in the `merge` function.
+ * 
+ * @param {Object} retObj 
+ * @param {Object} subObj 
+ * @param {string | number | symbol} key 
+ * @returns {*}
+ */
 const mergeCollision = (retObj, subObj, key) => subObj[key];
 
+/**
+ * Composes multiple functions together into one.
+ * 
+ * The order of function execution is right-most function to left-most function.
+ * 
+ * @param  {...Function} fns
+ * @returns {Function} 
+ */
 const compose = (...fns) => fns.reduce((acc, fn) => fn(acc));
 
+/**
+ * A function used in `ensureParams` to require its position exists but to always
+ * validate a `true`.
+ * 
+ * @returns {true}
+ */
 const exists = () => true;
+
+/**
+ * Used to normalize a valid path to be array based.
+ * 
+ * All strings are split by periods, numbers and symbols are made into one-element
+ * arrays. All else are returned as-is. In order for this last return to work, it
+ * assumes that `path` has already been validated with `isValidPath()`.
+ * 
+ * @param {string | number | symbol | Array<string | number | symbol>} path
+ * @returns {Array<string | number | symbol>} 
+ */
 const convertPathsToArray = path => {
     if ($STRING(path)) {
         return path.split('.'); 
@@ -98,8 +188,31 @@ const convertPathsToArray = path => {
 
     return path;
 };
+
+/**
+ * A decorator function for normalizing all parameters for traversal-based
+ * functions, such as `get()` or `set()`.
+ * 
+ * @param {Function}
+ * @returns {Function}
+ */
 const normalizePaths = normalizeParams(x => x, convertPathsToArray, x => x);
+
+/**
+ * A decorator function to guarantee that a function receives an [ object, valid-path ]
+ * argument signature.
+ * 
+ * @param {Function}
+ * @returns {Function}
+ */
 const hasObjectAndPath = ensureParams(() => undefined, isKeyed, isValidPath);
+
+/**
+ * A decorator function to guarantee that a function receives an [ object, valid-path, any ]
+ * argument signature.
+ * @param {Function}
+ * @returns {Function}
+ */
 const hasObjectPathAndValue = ensureParams(() => false, isKeyed, isValidPath, exists);
 
 module.exports = { compose, isKeyed, areObjects, isValidPath, _get, _set, _delete, _has, mergeCollision, convertPathsToArray, normalizePaths, hasObjectAndPath, hasObjectPathAndValue };
